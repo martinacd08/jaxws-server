715831ba79d1256b3b9feb25a70993343d00103b
martinacd08 <casandra123>
2015-05-22 11:26:33 -0700

    test cross domain

--- src/main/java/soap/service/HelloService.java
+++ src/main/java/soap/service/HelloService.java
@@ -2,17 +2,20 @@ package soap.service;
 
 import javax.jws.WebMethod;
 import javax.jws.WebService;
-
+import org.apache.cxf.rs.security.cors.CorsHeaderConstants;
+import org.apache.cxf.rs.security.cors.CrossOriginResourceSharing;
+import org.apache.cxf.rs.security.cors.LocalPreflight;
 /**
  * @author chamerling
  * 
  */
- @CrossOriginResourceSharing(
-        allowOrigins = true, 
-        allowCredentials = true, 
-        /*maxAge = 1,*/ 
-        allowHeaders = true, 
-        exposeHeaders = true
+ @CrossOriginResourceSharing(allowOrigins = {
+        "*"
+        }, allowCredentials = true, maxAge = 1, allowHeaders = {
+        "X-custom-1", "X-custom-2"
+        }, exposeHeaders = {
+        "X-custom-3", "X-custom-4"
+        }
 )
 @WebService
 public interface HelloService {
