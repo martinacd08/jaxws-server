741c98ee25fd1931edba2c60cdbfb0d8599f1555
martinacd08 <casandra123>
2015-05-22 11:39:15 -0700

    Revert "test cross domain"
    This reverts commit 715831ba79d1256b3b9feb25a70993343d00103b.

--- src/main/java/soap/service/HelloService.java
+++ src/main/java/soap/service/HelloService.java
@@ -2,20 +2,17 @@ package soap.service;
 
 import javax.jws.WebMethod;
 import javax.jws.WebService;
-import org.apache.cxf.rs.security.cors.CorsHeaderConstants;
-import org.apache.cxf.rs.security.cors.CrossOriginResourceSharing;
-import org.apache.cxf.rs.security.cors.LocalPreflight;
+
 /**
  * @author chamerling
  * 
  */
- @CrossOriginResourceSharing(allowOrigins = {
-        "*"
-        }, allowCredentials = true, maxAge = 1, allowHeaders = {
-        "X-custom-1", "X-custom-2"
-        }, exposeHeaders = {
-        "X-custom-3", "X-custom-4"
-        }
+ @CrossOriginResourceSharing(
+        allowOrigins = true, 
+        allowCredentials = true, 
+        /*maxAge = 1,*/ 
+        allowHeaders = true, 
+        exposeHeaders = true
 )
 @WebService
 public interface HelloService {
